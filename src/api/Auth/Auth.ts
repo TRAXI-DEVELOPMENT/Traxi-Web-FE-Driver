5c627c91dfe23ba1b609a69f0c0aa46de85b3ef0
unknown <nhoktmt2002@gmail.com>
2024-03-17 23:46:01 +0700

    Change Password

--- src/api/Auth/Auth.ts
+++ src/api/Auth/Auth.ts
@@ -1,14 +1,28 @@
-import requestWebDriver from "src/utils/axios";
+import requestWebDriver from 'src/utils/axios';
 
 export const loginDriver = async (phone: string, password: string) => {
-    try {
-      const response = await requestWebDriver.post('/api/v1/login/driver', {
-        phone,
-        password,
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error when calling loginDriver:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await requestWebDriver.post('/api/v1/login/driver', {
+      phone,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error when calling loginDriver:', error);
+    throw error;
+  }
+};
+
+export const changeDriverPassword = async (driverId: string, newPassword: string) => {
+  try {
+    console.log(driverId);
+    console.log(newPassword);
+    const response = await requestWebDriver.patch(`/api/v1/driver/password/${driverId}`, {
+      Password: newPassword,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error changing driver password:', error);
+    throw error;
+  }
+};
--- src/pages/demo/section/views/DriverProfile.tsx
+++ src/pages/demo/section/views/DriverProfile.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from 'react';
-import { Box, Typography, Stack, TextField } from '@mui/material';
+import { Box, Typography, Stack, TextField, Button } from '@mui/material';
 import { useDriverInfo } from 'src/hooks/useDriverInfo';
 import { getDriverInfo } from 'src/api/Driver/Driver';
 import EcommerceAccountLayout from '../../account/layouts/AccountLayout';
 import DriverLicenseCard from '../../components/navbar/DriverLicenseCard';
+import { changeDriverPassword } from 'src/api/Auth/Auth';
+import { Snackbar, Alert } from '@mui/material';
+import { AlertColor } from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 // ----------------------------------------------------------------------
 
 export default function DriverProfile() {
+  const [password, setPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+  const [showChangePasswordForm, setShowChangePasswordForm] = useState(false);
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor | undefined>('info');
+
   const driverId = useDriverInfo();
 
   const [driverData, setDriverData] = useState({
@@ -25,7 +38,13 @@ export default function DriverProfile() {
     if (driverId) {
       getDriverInfo(driverId)
         .then((data) => {
+          console.log('Data received from getDriverInfo:', data);
           const { result } = data;
+          if (result && result.Password) {
+            setPassword(result.Password);
+          } else {
+            console.log('Password field is missing in the response');
+          }
           if (result) {
             setDriverData({
               fullName: result.FullName,
@@ -41,6 +60,48 @@ export default function DriverProfile() {
     }
   }, [driverId]);
 
+  const isValidPassword = (password: string): boolean => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
+
+  const handleChangePassword = async () => {
+    console.log('password', password);
+    if (oldPassword !== password) {
+      setSnackbarMessage('Mật khẩu cũ không đúng.');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+      return;
+    }
+
+    if (!isValidPassword(newPassword)) {
+      setSnackbarMessage(
+        'Mật khẩu mới phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và kí tự đặc biệt.'
+      );
+      setSnackbarSeverity('warning');
+      setSnackbarOpen(true);
+      return;
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      setSnackbarMessage('Mật khẩu mới và xác nhận mật khẩu mới không khớp.');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+      return;
+    }
+
+    try {
+      await changeDriverPassword(driverId, newPassword);
+      setSnackbarMessage('Mật khẩu đã được thay đổi thành công.');
+      setSnackbarSeverity('success');
+    } catch (error) {
+      console.error('Lỗi khi thay đổi mật khẩu:', error);
+      setSnackbarMessage('Có lỗi xảy ra khi thay đổi mật khẩu.');
+      setSnackbarSeverity('error');
+    } finally {
+      setSnackbarOpen(true);
+    }
+  };
   return (
     <EcommerceAccountLayout>
       <Box
@@ -84,7 +145,77 @@ export default function DriverProfile() {
       </Box>
 
       <Stack spacing={3} sx={{ my: 5 }}>
-        <Typography variant="h5"> Change Password </Typography>
+        <Typography
+          variant="h5"
+          onClick={() => setShowChangePasswordForm(!showChangePasswordForm)}
+          style={{ cursor: 'pointer' }}
+        >
+          Đổi mật khẩu
+        </Typography>
+        {showChangePasswordForm && (
+          <>
+            <TextField
+              label="Mật khẩu cũ"
+              variant="outlined"
+              type={showPassword ? 'text' : 'password'}
+              value={oldPassword}
+              onChange={(e) => setOldPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              label="Mật khẩu mới"
+              variant="outlined"
+              type={showPassword ? 'text' : 'password'}
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              label="Xác nhận mật khẩu mới"
+              variant="outlined"
+              type={showPassword ? 'text' : 'password'}
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button variant="contained" onClick={handleChangePassword}>
+              Đổi Mật Khẩu
+            </Button>
+          </>
+        )}
         <DriverLicenseCard degreeId={driverId} />
         {/* <Stack spacing={2.5}>
               <RHFTextField
@@ -133,6 +264,15 @@ export default function DriverProfile() {
               />
             </Stack> */}
       </Stack>
+      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={() => setSnackbarOpen(false)}>
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity={snackbarSeverity}
+          sx={{ width: '100%' }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </EcommerceAccountLayout>
   );
 }
